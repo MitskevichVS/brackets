b823886d81c6204ceb89b68731887e4e0c5371f7
Mitskevich Victor <mv2.mx@yandex.ru>
2019-09-23 12:35:30 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) {
+      return false;
+  }
+
+  let openBr = [];
+  let closeBr = [];
+  let stack = [];
+  const procStr = str.split('');
+  
+  bracketsConfig.map(item => {
+    openBr.push(item[0]);
+    closeBr.push(item[1]);
+  });
+
+  procStr.forEach(element => {
+    if (closeBr.includes(element) && stack.length === 0 && element !== '|') {
+      stack.push('false');
+    } else if (closeBr.includes(element) && openBr.indexOf(stack[stack.length - 1]) === closeBr.indexOf(element)) {
+      stack.pop();
+    } else if (openBr.includes(element)) {
+      stack.push(element);
+    } 
+  });
+
+  if (stack.length === 0) {
+      return true;
+    } else return false;
 }
